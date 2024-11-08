bcdc2e4d038aa1952161ad02de3f57516dc7928d
muhammedalfaris <muhammedalfariz8003@gmail.com>
2024-11-08 14:44:29 +0530

    merged

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
@@ -34,7 +11,6 @@ function App() {
         </Routes>
       </BrowserRouter>
     </>
->>>>>>> develop
   );
 }
 
