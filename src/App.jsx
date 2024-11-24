98010db41914e6bfc4c3f8b4f7b7883d3a791cda
NANDANA8891 <nandanaayyappan0@gmail.com>
2024-11-24 21:58:30 +0530

    admin login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-
+import AdminLogin from './components/AdminLogin';
+import ForgotPassword from './components/ForgotPassword';
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage/>} />
+          <Route path='/adminlogin' element={<AdminLogin/>}/>
+          <Route path='/forgotpassword' element={<ForgotPassword/>}/>
         </Routes>
       </BrowserRouter>
     </>
