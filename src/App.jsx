38f85f4f7e027ff75075983d40cf6381c3888a30
simi <simim901@gmail.com>
2024-11-25 07:55:17 +0530

    user login

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Loginpage from "./components/Loginpage";
+ import Loginpage from "./components/Loginpage";
+ import RegistrationForm  from "./components/RegistrationForm";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Loginpage />} />
+           
+           <Route path="/" element={<Loginpage />} /> 
+            {/* <Route path="/r" element={<RegistrationForm />}/>    */}
         </Routes>
       </BrowserRouter>
     </>
   );
 }
-
 export default App;
