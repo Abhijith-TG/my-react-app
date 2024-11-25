ed806c540073a8c14dfb33404ac3a2a58cf9088f
HansaAbraham <hansaabraham143@gmail.com>
2024-11-25 13:51:08 +0530

    Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,18 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import UserProfile from './components/Profile';
+import './style/Profile.css'
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage/>} />
+          <Route path='/' element={<UserProfile/>} />
         </Routes>
       </BrowserRouter>
     </>
->>>>>>> develop
   );
 }
 
