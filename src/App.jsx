adc6e330d7257335bda869bcff4a32eb0579ac12
alameen1514 <alamee1514@gmail.com>
2024-11-27 09:38:59 +0530

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductSearch from './components/Navbar'; 
+import SearchResults from './components/SearchResults'; 
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage/>} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductSearch />} />
+        <Route path="/search" element={<SearchResults />} />
+      </Routes>
+    </Router>
   );
 }
 
