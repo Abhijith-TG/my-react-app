6e1e20c437199eee77d47b0c6ddf7be39d75eab6
muhammedalfaris <muhammedalfariz8003@gmail.com>
2024-11-28 14:11:15 +0530

    adding home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchResults from './components/SearchResults'; 
 import RegistrationForm from './components/signup_page';
 import Home from './components/Home';
 import './styles/home.css'
@@ -12,11 +11,9 @@ function App() {
     <Router>
       <ApiFetchContext>
       <Routes>
-      <Route path='/' element={<RegistrationForm/>} />
-      <Route path='/home' element={<Home/>} />
+      <Route path='/sign' element={<RegistrationForm/>} />
+      <Route path='/' element={<Home/>} />
       <Route path='/category/:categoryName' element={<Home/>} />
-      <Route path="/product-search" element={<ProductSearch />} />
-      <Route path="/search" element={<SearchResults />} />
       </Routes>
       </ApiFetchContext>
     </Router>
